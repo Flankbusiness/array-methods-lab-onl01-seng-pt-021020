d86c45169dc18998b571c73d99554cb5089b8f94
Steven Prindle <flankbusiness@gmail.com>
2020-02-18 03:54:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,11 +4,10 @@ end
 
 def using_sort(array)
 ["wow", "I", "am", "really", "learning", "arrays!"].sort
-
 end
 
 def using_reverse(array)
-
+["wow", "I", "am", "really", "learning", "arrays!"].reverse
 end
 
 def using_first(array)
