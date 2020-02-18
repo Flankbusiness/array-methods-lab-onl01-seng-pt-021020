1224620db94646ec76c3a3351899b416c0ec41ad
Steven Prindle <flankbusiness@gmail.com>
2020-02-18 03:54:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-["wow", "I", "am", "really", "learning", "arrays!"]
+["wow", "I", "am", "really", "learning", "arrays!"].sort
 
 end
 
